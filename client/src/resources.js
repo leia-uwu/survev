6fb239c6abed8191485e25509700a331622f8a7b
leia uwu <leia@tutamail.com>
2024-03-03 10:40:05 -0300

    refactor(client): cleanup resources.js

--- client/src/resources.js
+++ client/src/resources.js
@@ -5,219 +5,259 @@ import SoundDefs from "./soundDefs";
 import lowResAtlasDefs from "./lowResAtlasDefs";
 import fullResAtlasDefs from "./fullResAtlasDefs";
 
-const AtlasDefs = {
+const spritesheetDefs = {
     low: lowResAtlasDefs,
     high: fullResAtlasDefs
 };
 
-function i(e, t) {
-    const r = PIXI.Texture.from(t);
-    const a = r.baseTexture;
-    let i = 0;
-    if (!a.hasLoaded) {
-        a.on("loaded", (r) => {
-            console.log("Loaded texture", t);
-            e.prepare.upload(r);
+/**
+ * @param {PIXI.Renderer} renderer
+ * @param {string} url
+ */
+function loadTexture(renderer, url) {
+    const tex = PIXI.Texture.from(url);
+    const baseTex = tex.baseTexture;
+    let loadAttempts = 0;
+
+    if (!baseTex.hasLoaded) {
+        baseTex.on("loaded", (baseTex) => {
+            console.log("Loaded texture", url);
+            renderer.prepare.upload(baseTex);
         });
-        a.on("error", (e) => {
-            console.log("BaseTex load error, retrying", t);
-            if (i++ <= 3) {
+
+        baseTex.on("error", (baseTex) => {
+            console.log("BaseTex load error, retrying", url);
+            if (loadAttempts++ <= 3) {
                 setTimeout(
                     () => {
-                        if (e.source) {
-                            e.updateSourceImage("");
-                            e.updateSourceImage(
-                                t.substring(5, t.length)
+                        if (baseTex.source) {
+                            baseTex.updateSourceImage("");
+                            baseTex.updateSourceImage(
+                                url.substring(5, url.length)
                             );
                         }
                     },
-                    (i - 1) * 1000
+                    (loadAttempts - 1) * 1000
                 );
             }
         });
     }
-    return a;
+    return baseTex;
 }
-function o(e, t) {
-    const r = i(e, `assets/${t.meta.image}`);
-    const a = new PIXI.Spritesheet(r, t);
-    a.resolution = r.resolution;
-    a.parse();
-    return a;
+
+/**
+ * @param {PIXI.Renderer} renderer
+ * @param {PIXI.ISpritesheetData} data
+ */
+function loadSpritesheet(renderer, data) {
+    const baseTex = loadTexture(renderer, `assets/${data.meta.image}`);
+
+    const sheet = new PIXI.Spritesheet(baseTex, data);
+    sheet.resolution = baseTex.resolution;
+    sheet.parse();
+
+    return sheet;
 }
-function s(e, t) {
-    let r = Math.min(window.screen.width, window.screen.height);
-    let a = Math.max(window.screen.width, window.screen.height);
-    r *= window.devicePixelRatio;
-    a *= window.devicePixelRatio;
-    const i = a < 1366 && r < 768;
-    let o = t.get("highResTex") ? "high" : "low";
+
+/**
+ * @param {PIXI.Renderer} renderer
+ */
+function selectTextureRes(renderer, config) {
+    let minDim = Math.min(window.screen.width, window.screen.height);
+    let maxDim = Math.max(window.screen.width, window.screen.height);
+    minDim *= window.devicePixelRatio;
+    maxDim *= window.devicePixelRatio;
+    const smallScreen = maxDim < 1366 && minDim < 768;
+    let textureRes = config.get("highResTex") ? "high" : "low";
+
     if (
-        i ||
+        smallScreen ||
         (device.mobile && !device.tablet) ||
-        e.type == PIXI.RENDERER_TYPE.CANVAS
+        renderer.type == PIXI.RENDERER_TYPE.CANVAS
     ) {
-        o = "low";
+        textureRes = "low";
     }
-    if (e.type == PIXI.RENDERER_TYPE.WEBGL) {
-        const s = e.gl;
+    if (renderer.type == PIXI.RENDERER_TYPE.WEBGL) {
+        const s = renderer.gl;
         if (s.getParameter(s.MAX_TEXTURE_SIZE) < 4096) {
-            o = "low";
+            textureRes = "low";
         }
     }
     console.log(
         "TextureRes",
-        o,
+        textureRes,
         "screenDims",
         window.screen.width,
         window.screen.height
     );
-    return o;
+    return textureRes;
 }
 
 class ResourceManager {
+    /**
+    * @param {PIXI.Renderer} renderer
+    */
     constructor(renderer, audioManager, config) {
         this.renderer = renderer;
         this.audioManager = audioManager;
         this.config = config;
-        this.textureRes = s(this.renderer, this.config);
+        this.textureRes = selectTextureRes(this.renderer, this.config);
         this.atlases = {};
         this.loadTicker = 0;
         this.loaded = false;
         renderer.prepare.limiter.maxItemsPerFrame = 1;
     }
 
-    isAtlasLoaded(e) {
-        return this.atlases[e]?.loaded;
+    isAtlasLoaded(name) {
+        return this.atlases[name]?.loaded;
     }
 
-    atlasTexturesLoaded(e) {
-        if (!this.isAtlasLoaded(e)) {
+    atlasTexturesLoaded(name) {
+        if (!this.isAtlasLoaded(name)) {
             return false;
         }
-        for (
-            let t = this.atlases[e], r = 0;
-            r < t.spritesheets.length;
-            r++
-        ) {
-            const a = t.spritesheets[r];
-            if (!a.baseTexture?.hasLoaded) {
+
+        const atlas = this.atlases[name];
+        for (let i = 0; i < atlas.spritesheets.length; i++) {
+            const spritesheet = atlas.spritesheets[i];
+            if (!spritesheet.baseTexture?.hasLoaded) {
                 return false;
             }
         }
+
         return true;
     }
 
-    loadAtlas(e) {
-        if (!this.isAtlasLoaded(e)) {
-            console.log("Load atlas", e);
-            this.atlases[e] = this.atlases[e] || {
-                loaded: false,
-                spritesheets: []
-            };
-            for (
-                let t = AtlasDefs[this.textureRes] || AtlasDefs.low,
-                    r = t[e],
-                    a = 0;
-                a < r.length;
-                a++
-            ) {
-                const i = o(this.renderer, r[a]);
-                this.atlases[e].spritesheets.push(i);
-            }
-            this.atlases[e].loaded = true;
+    loadAtlas(name) {
+        if (this.isAtlasLoaded(name)) {
+            return;
         }
+
+        console.log("Load atlas", name);
+
+        this.atlases[name] = this.atlases[name] || {
+            loaded: false,
+            spritesheets: []
+        };
+
+        const atlasDefs = spritesheetDefs[this.textureRes] || spritesheetDefs.low;
+        const atlasDef = atlasDefs[name];
+        for (let i = 0; i < atlasDef.length; i++) {
+            const atlas = loadSpritesheet(this.renderer, atlasDef[i]);
+            this.atlases[name].spritesheets.push(atlas);
+        }
+        this.atlases[name].loaded = true;
     }
 
-    unloadAtlas(e) {
-        if (this.isAtlasLoaded(e)) {
-            console.log("Unload atlas", e);
-            const atlas = this.atlases[e];
-            for (let i = 0; i < atlas.spritesheets.length; i++) {
-                atlas.spritesheets[i].destroy(true);
-            }
-            atlas.loaded = false;
-            atlas.spritesheets = [];
+    unloadAtlas(name) {
+        if (!this.isAtlasLoaded(name)) {
+            return;
         }
+
+        console.log("Unload atlas", name);
+
+        const atlas = this.atlases[name];
+        for (let i = 0; i < atlas.spritesheets.length; i++) {
+            atlas.spritesheets[i].destroy(true);
+        }
+        atlas.loaded = false;
+        atlas.spritesheets = [];
     }
 
-    loadMapAssets(e) {
-        const t = this;
-        console.log("Load map", e);
-        const r = MapDefs[e];
-        if (!r) {
-            throw new Error(
-                `Failed loading mapDef ${this.mapName}`
-            );
+    loadMapAssets(mapName) {
+        console.log("Load map", mapName);
+
+        const mapDef = MapDefs[mapName];
+        if (!mapDef) {
+            throw new Error(`Failed loading mapDef ${this.mapName}`);
         }
-        const a = r.assets.atlases;
-        for (
-            let i = Object.keys(this.atlases), o = 0;
-            o < i.length;
-            o++
-        ) {
-            const s = i[o];
-            if (a.indexOf(s) === -1) {
-                this.unloadAtlas(s);
+
+        //
+        // Textures
+        //
+        const atlasList = mapDef.assets.atlases;
+
+        // Unload all atlases that aren't in the new list
+        const keys = Object.keys(this.atlases);
+        for (let i = 0; i < keys.length; i++) {
+            const key = keys[i];
+            if (atlasList.indexOf(key) === -1) {
+                this.unloadAtlas(key);
             }
         }
-        for (let n = 0; n < a.length; n++) {
-            const l = a[n];
-            if (!this.isAtlasLoaded(l)) {
-                this.loadAtlas(l);
+
+        // Load all new atlases
+        for (let _i = 0; _i < atlasList.length; _i++) {
+            const atlas = atlasList[_i];
+            if (!this.isAtlasLoaded(atlas)) {
+                this.loadAtlas(atlas);
             }
         }
+
+        //
+        // Audio
+        //
+        // PIXI spritesheets internally defer loading textures if the spritesheet
+        // has more than 1000 images as a part of its internal batching process.
+        //
+        // Because we want images to load before audio, we'll also defer loading
+        // audio in a similar fashion.
         setTimeout(() => {
-            t.audioManager.preloadSounds();
-            for (
-                let e = r.assets.audio, a = 0;
-                a < e.length;
-                a++
-            ) {
-                const i = e[a];
-                let o = SoundDefs.Sounds[i.channel];
-                if (!o) {
-                    const s = SoundDefs.Channels[i.channel];
-                    o = SoundDefs.Sounds[s.list];
+            // Load shared audio
+            this.audioManager.preloadSounds();
+
+            // Load audio specific to the map
+            const soundList = mapDef.assets.audio;
+            for (let i = 0; i < soundList.length; i++) {
+                const sound = soundList[i];
+
+                // @HACK: Sometimes the channel doesn't correspond to where
+                // the sound is defined in sound-defs.js; this is the case
+                // with "players" sounds. Use an alternate method for looking
+                // up the sound def.
+                let soundsList = SoundDefs.Sounds[sound.channel];
+                if (!soundsList) {
+                    const channelDef = SoundDefs.Channels[sound.channel];
+                    soundsList = SoundDefs.Sounds[channelDef.list];
                 }
-                const n = o[i.name];
-                const l = {
-                    canCoalesce: n.canCoalesce,
-                    channels: n.maxInstances,
-                    volume: n.volume
+
+                const soundDef = soundsList[sound.name];
+
+                const options = {
+                    canCoalesce: soundDef.canCoalesce,
+                    channels: soundDef.maxInstances,
+                    volume: soundDef.volume
                 };
-                t.audioManager.loadSound({
-                    name: i.name,
-                    channel: i.channel,
-                    path: n.path,
-                    options: l
+
+                this.audioManager.loadSound({
+                    name: sound.name,
+                    channel: sound.channel,
+                    path: soundDef.path,
+                    options
                 });
             }
         }, 0);
     }
 
-    update(e) {
+    update(dt) {
+        // Debug
         if (!this.loaded) {
-            this.loadTicker += e;
-            let t = !this.preloadMap;
-            for (
-                let r = Object.keys(this.atlases), a = 0;
-                a < r.length;
-                a++
-            ) {
-                const i = r[a];
-                if (!this.atlasTexturesLoaded(i)) {
-                    t = false;
+            this.loadTicker += dt;
+
+            let loaded = !this.preloadMap;
+            const keys = Object.keys(this.atlases);
+            for (let i = 0; i < keys.length; i++) {
+                const key = keys[i];
+                if (!this.atlasTexturesLoaded(key)) {
+                    loaded = false;
                 }
             }
             if (!this.audioManager.allLoaded()) {
-                t = false;
+                loaded = false;
             }
-            if (t) {
-                console.log(
-                    "Resource load complete",
-                    this.loadTicker.toFixed(2)
-                );
+
+            if (loaded) {
+                console.log("Resource load complete", this.loadTicker.toFixed(2));
                 this.loaded = true;
             }
         }
