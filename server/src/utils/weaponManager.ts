16574fbcf2b1db5b184f31e047f0fd7ffba40f90
leia uwu <leia@tutamail.com>
2024-06-20 19:39:01 -0300

    temporary disable throwing throwables

--- server/src/utils/weaponManager.ts
+++ server/src/utils/weaponManager.ts
@@ -167,10 +167,10 @@ export class WeaponManager {
                 this.fireWeapon();
                 break;
             }
-            case "throwable": {
-                this.cookThrowable();
-                break;
-            }
+            // case "throwable": {
+            //     this.cookThrowable();
+            //     break;
+            // }
             }
         }
     }
