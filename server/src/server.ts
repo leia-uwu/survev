798cc07619ec341e467ef0d26de84cf28b05cdf0
MamounKolovos <mamoun.kolovos@gmail.com>
2024-07-10 20:58:08 -0400

    fix compile time error

--- server/src/server.ts
+++ server/src/server.ts
@@ -215,7 +215,7 @@ export class Server {
         try {
             game.handleMsg(message, data);
         } catch (e) {
-            this.game.logger.warn("Error parsing message:", e);
+            game.logger.warn("Error parsing message:", e);
         }
     }
 
