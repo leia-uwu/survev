6d5930be4af3a3ee319841a0485bc8a62cf62af0
lmssiehdev <122186255+lmssiehdev@users.noreply.github.com>
2024-08-13 19:28:05 +0100

    map modifications

--- server/src/deathmatch/maps/main.ts
+++ server/src/deathmatch/maps/main.ts
@@ -8,6 +8,9 @@ const config = {
 } as const;
 
 export const DeatchmatchMain: MapDef = util.mergeDeep(structuredClone(Main), {
+    biome: {
+        particles: { camera: "falling_leaf_spring" }
+    },
     gameConfig: {
         planes: {
             timings: [
@@ -21,63 +24,61 @@ export const DeatchmatchMain: MapDef = util.mergeDeep(structuredClone(Main), {
     },
     mapGen: {
         map: {
-            baseWidth: 280,
-            baseHeight: 280,
+            baseWidth: 250,
+            baseHeight: 250,
+            shoreInset: 40,
             rivers: {
                 weights: []
             }
         },
         places: Main.mapGen
             ? Array(config.places)
-                  .fill(false)
-                  .map(() => {
-                      return Main.mapGen?.places[
-                          Math.floor(Math.random() * Main.mapGen.places.length)
-                      ];
-                  })
+                .fill(false)
+                .map(() => {
+                    return Main.mapGen?.places[
+                        Math.floor(Math.random() * Main.mapGen.places.length)
+                    ];
+                })
             : {},
         densitySpawns: Main.mapGen
             ? Main.mapGen.densitySpawns.reduce(
-                  (array, item) => {
-                      let object: Record<string, number> = {};
-                      for (const [key, value] of Object.entries(item)) {
-                          object[key] = (value * 32) / 100;
-                      }
-                      array.push(object);
-                      return array;
-                  },
-                  [] as Record<string, number>[]
-              )
+                (array, item) => {
+                    let object: Record<string, number> = {};
+                    for (const [key, value] of Object.entries(item)) {
+                        object[key] = (value * 32) / 100;
+                    }
+                    array.push(object);
+                    return array;
+                },
+                [] as Record<string, number>[]
+            )
             : {},
         fixedSpawns: [
             {
                 // small is spawn count for solos and duos, large is spawn count for squads
                 warehouse_01: 1,
-                house_red_01: { small: 1, large: 1 },
+                house_red_01: 1,
                 // house_red_02: { small: 1, large: 1 },
                 // barn_01: { small: 1, large: 3 },
                 // barn_02: 1,
                 hut_01: 2,
                 hut_02: 1, // spas hut
                 hut_03: 1, // scout hut
-                greenhouse_01: { small: 1, large: 1 },
+                greenhouse_01: 1,
                 cache_01: 1,
                 cache_02: { odds: 0.1 }, // mosin tree
                 cache_07: 1,
                 // bunker_structure_01: { odds: 0.05 },
-                // bunker_structure_02: 1,
+                bunker_structure_02: 1,
                 // bunker_structure_03: 1,
                 // bunker_structure_04: 1,
                 // bunker_structure_05: 1,
                 // warehouse_complex_01: 1,
                 chest_01: 1,
                 chest_03: { odds: 0.2 },
-                mil_crate_02: { odds: 0.25 },
+                // mil_crate_02: { odds: 0.25 },
                 tree_02: 3,
-                teahouse_complex_01su: {
-                    small: 1,
-                    large: 1
-                },
+                teahouse_complex_01su: { odds: 0.5 },
                 // stone_04: 1,
                 club_complex_01: 1
             }
@@ -95,3 +96,42 @@ export const DeatchmatchMain: MapDef = util.mergeDeep(structuredClone(Main), {
         ]
     }
 });
+
+DeatchmatchMain["lootTable"] = {
+    tier_mansion_floor: [{ name: "outfitCasanova", count: 1, weight: 1 }],
+    tier_vault_floor: [{ name: "outfitJester", count: 1, weight: 1 }],
+    tier_police_floor: [{ name: "outfitPrisoner", count: 1, weight: 1 }],
+    tier_chrys_01: [{ name: "outfitImperial", count: 1, weight: 1 }],
+    tier_chrys_02: [{ name: "katana", count: 1, weight: 1 }],
+    tier_chrys_case: [
+        // { name: "helmet03_forest", count: 1, weight: 199 },
+        { name: "tier_katanas", count: 1, weight: 3 },
+        { name: "naginata", count: 1, weight: 1 }
+    ],
+    tier_eye_02: [{ name: "stonehammer", count: 1, weight: 1 }],
+    tier_eye_block: [
+        { name: "m9", count: 1, weight: 1 },
+        { name: "ots38_dual", count: 1, weight: 1 },
+        { name: "flare_gun", count: 1, weight: 1 },
+        { name: "colt45", count: 1, weight: 1 },
+        { name: "45acp", count: 1, weight: 1 },
+        { name: "painkiller", count: 1, weight: 1 },
+        { name: "m4a1", count: 1, weight: 1 },
+        { name: "m249", count: 1, weight: 1 },
+        { name: "awc", count: 1, weight: 1 },
+        { name: "pkp", count: 1, weight: 1 }
+    ],
+    tier_sledgehammer: [{ name: "sledgehammer", count: 1, weight: 1 }],
+    tier_chest_04: [
+        { name: "p30l", count: 1, weight: 40 },
+        { name: "p30l_dual", count: 1, weight: 1 }
+    ],
+    tier_woodaxe: [{ name: "woodaxe", count: 1, weight: 1 }],
+    tier_club_melee: [{ name: "machete_taiga", count: 1, weight: 1 }],
+    tier_pirate_melee: [{ name: "hook", count: 1, weight: 1 }],
+    tier_hatchet_melee: [
+        { name: "fireaxe", count: 1, weight: 5 },
+        { name: "tier_katanas", count: 1, weight: 3 },
+        { name: "stonehammer", count: 1, weight: 1 }
+    ],
+}
