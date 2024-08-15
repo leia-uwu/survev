f33da420443a3a71a52a4ee6a51dca54d9c3cd96
lmssiehdev <122186255+lmssiehdev@users.noreply.github.com>
2024-08-15 20:56:35 +0100

    give nades on kill

--- server/src/plugins/deathMatch.ts
+++ server/src/plugins/deathMatch.ts
@@ -1,9 +1,5 @@
-import { GameObjectDefs } from "../../../shared/defs/gameObjectDefs";
-import type { GunDef } from "../../../shared/defs/gameObjects/gunDefs";
 import { WeaponSlot } from "../../../shared/gameConfig";
 import { ObjectType } from "../../../shared/net/objectSerializeFns";
-import { math } from "../../../shared/utils/math";
-import { Emote } from "../game/objects/player";
 import { GamePlugin } from "../game/pluginManager";
 
 export default class DeathMatchPlugin extends GamePlugin {
@@ -45,42 +41,9 @@ export default class DeathMatchPlugin extends GamePlugin {
             // give the killer health and gun ammo and inventory ammo
             if (data.source?.__type === ObjectType.Player) {
                 const killer = data.source;
-                killer.health += 20;
-                killer.boost += 25;
-
-                function calculateAmmoToGive(
-                    currAmmo: number,
-                    maxClip: number,
-                    amount = 50
-                ) {
-                    const percentage = (maxClip * amount) / 100;
-                    return math.clamp(currAmmo + percentage, 0, maxClip);
-                }
-
-                const primary = {
-                    ...killer.weapons[WeaponSlot.Primary]
-                };
-
-                if (primary.type != "") {
-                    const primaryGunDef = GameObjectDefs[primary.type] as GunDef;
-                    killer.weapons[WeaponSlot.Primary] = {
-                        ...primary,
-                        ammo: calculateAmmoToGive(primary.ammo, primaryGunDef.maxClip)
-                    };
-                }
-
-                const secondary = {
-                    ...killer.weapons[WeaponSlot.Secondary]
-                };
-
-                if (secondary.type != "") {
-                    const secondaryGunDef = GameObjectDefs[secondary.type] as GunDef;
-
-                    killer.weapons[WeaponSlot.Secondary] = {
-                        ...secondary,
-                        ammo: calculateAmmoToGive(secondary.ammo, secondaryGunDef.maxClip)
-                    };
-                }
+                killer.inventory["frag"] += 3;
+                killer.inventory["mirv"] += 1;
+                killer.inventoryDirty = true;
             }
         });
     }
