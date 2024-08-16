55fe2ea2a0582a4740079647d45e9ef6fb9be705
lmssiehdev <122186255+lmssiehdev@users.noreply.github.com>
2024-08-16 15:58:09 +0100

    fix getParameterByName helper function

--- client/src/device.ts
+++ client/src/device.ts
@@ -1,9 +1,5 @@
 import { isMobile } from "pixi.js-legacy";
-
-function getParameterByName(name: string, url?: string) {
-    const searchParams = new URLSearchParams(url || window.location.search);
-    return searchParams.get(name) || "";
-}
+import { getParameterByName } from "./helpers";
 
 function detectMobile() {
     return isMobile.android.device || isMobile.apple.device || isIpad();
