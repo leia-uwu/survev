144a634b4bc1308c64b8d662a645b1a145e4bb9c
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-03-08 19:25:36 +0000

    feat: add caching with redis

---
+++ server/src/api/cache.ts
@@ -0,0 +1,33 @@
+import { createClient } from 'redis';
+
+type RedisClientType = ReturnType<typeof createClient>;
+
+let redisClient: RedisClientType;
+
+export async function getRedisClient() {
+	if (redisClient) {
+		return redisClient;
+	}
+	const cacheInstance = createClient();
+
+	cacheInstance.on("connect", () => console.log("Connected to redis"));
+	process.on('exit', () => cleanupRedis);
+
+	await cacheInstance.connect();
+	redisClient = cacheInstance;
+	return cacheInstance;
+}
+
+export async function cleanupRedis() {
+	if (redisClient) {
+		await redisClient
+			.disconnect()
+			.catch((err) =>
+				console.log(
+					`CacheStore - Error while disconnecting from redis: ${
+						err instanceof Error ? err.message : 'Unknown error'
+					}`,
+				),
+			);
+	}
+}
