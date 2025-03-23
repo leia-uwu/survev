2ceff952e0780ed0ac815a3dc984cdd8f6dd328f
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-03-23 05:17:30 +0000

    chore(lint): damn it

--- server/src/api/routes/user/AuthRouter.ts
+++ server/src/api/routes/user/AuthRouter.ts
@@ -1,12 +1,12 @@
 import { Hono } from "hono";
+import { accountsEnabledMiddleware } from "../../auth/middleware";
 import { DiscordRouter } from "./auth/discord";
 import { GoogleRouter } from "./auth/google";
 import { MockRouter } from "./auth/mock";
-import { accountsEnabledMiddleware } from "../../auth/middleware";
 
 export const AuthRouter = new Hono();
 
-AuthRouter.use(accountsEnabledMiddleware)
+AuthRouter.use(accountsEnabledMiddleware);
 
 AuthRouter.route("/discord", DiscordRouter);
 AuthRouter.route("/google", GoogleRouter);
