520a3ef2ac1a6460357cb77d8776a2eb5c2e68ae
MamounKolovos <mamoun.kolovos@gmail.com>
2025-05-05 13:08:24 -0400

    refactor: simplify killfeedmsg logic

--- shared/net/killFeedMsg.ts
+++ shared/net/killFeedMsg.ts
@@ -7,17 +7,12 @@ export class KillFeedMsg implements AbstractMsg {
     serialize(s: BitStream) {
         s.writeASCIIString(this.text);
         s.writeAlignToNextByte();
-        s.writeUint8((this.color >> 16) & 0xff); //red
-        s.writeUint8((this.color >> 8) & 0xff); //green
-        s.writeUint8(this.color & 0xff); //blue
+        s.writeBits(this.color, 24);
     }
 
     deserialize(s: BitStream) {
         this.text = s.readASCIIString();
         s.readAlignToNextByte();
-        this.color =
-            (s.readUint8() << 16) | //red
-            (s.readUint8() << 8) | //green
-            s.readUint8(); //blue
+        this.color = s.readBits(24);
     }
 }
