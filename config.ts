06b9ddb029c7dafa975b80e5d193564019cd0297
leia uwu <leia@tutamail.com>
2025-05-08 14:51:23 -0300

    fix: turn off unique names by default

--- config.ts
+++ config.ts
@@ -61,7 +61,7 @@ export function getConfig(isProduction: boolean, dir: string) {
         captchaEnabled: false,
         cachingEnabled: false,
         rateLimitsEnabled: isProduction,
-        uniqueInGameNames: true,
+        uniqueInGameNames: false,
         debug: {
             spawnMode: "default",
             allowBots: isDev,
